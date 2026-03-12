e9756f12869784005719bde4bede6a7c0fd02149
François Billet <francois.billet.ext@veolia.com>
2026-03-12 11:57:06 +0100

    fix: add pieces I play & remove unused files

--- src/data/pieces.ts
+++ src/data/pieces.ts
@@ -3,21 +3,81 @@ export interface Piece {
   composer: string;
 }
 
-// Ajoutez ou modifiez vos morceaux ici facilement !
 export const pieces: Piece[] = [
-  { title: "Clair de Lune", composer: "Claude Debussy" },
-  { title: "Gymnopédie No.1", composer: "Erik Satie" },
-  { title: "Gymnopédie No.2", composer: "Erik Satie" },
-  { title: "Gnossienne No.1", composer: "Erik Satie" },
-  { title: "Sonate au Clair de Lune", composer: "Ludwig van Beethoven" },
+  { title: "Una mattina", composer: "Ludovico Einaudi" },
+  { title: "Primavera", composer: "Ludovico Einaudi" },
+  { title: "Nuvole bianche", composer: "Ludovico Einaudi" },
+
+  { title: "River flows in you", composer: "Yiruma" },
+  { title: "Love me", composer: "Yiruma" },
+  { title: "Maybe", composer: "Yiruma" },
+  { title: "Kiss the rain", composer: "Yiruma" },
+  { title: "Scenery through my window", composer: "Yiruma" },
+
+  { title: "Summer", composer: "Joe Hisaishi" },
+  { title: "Le voyage de Chihiro", composer: "Joe Hisaishi" },
+  { title: "The wind forest (Totoro)", composer: "Joe Hisaishi" },
+  { title: "Le château dans le ciel", composer: "Joe Hisaishi" },
+  { title: "Le château ambulant", composer: "Joe Hisaishi" },
+
+  { title: "The heart asks pleasure first", composer: "Michael Nyman" },
+
+  { title: "Comptine d'un autre été: l'après-midi", composer: "Yann Tiersen" },
+  { title: "La valse d'Amélie", composer: "Yann Tiersen" },
+  { title: "Rue des cascades", composer: "Yann Tiersen" },
+  { title: "Mother's journey", composer: "Yann Tiersen" },
+
+  { title: "Sonate en do", composer: "Mozart" },
+  { title: "Marche turque", composer: "Mozart" },
+
+  { title: "Pélude en do", composer: "Johann Sebastian Bach" },
+
   { title: "Lettre à Élise", composer: "Ludwig van Beethoven" },
-  { title: "Prélude en Do majeur", composer: "Johann Sebastian Bach" },
-  { title: "Nocturne Op.9 No.2", composer: "Frédéric Chopin" },
-  { title: "Valse en La mineur", composer: "Frédéric Chopin" },
-  { title: "Rêverie", composer: "Claude Debussy" },
-  { title: "Arabesque No.1", composer: "Claude Debussy" },
-  { title: "La Campanella", composer: "Franz Liszt" },
-  { title: "Consolation No.3", composer: "Franz Liszt" },
-  { title: "Träumerei", composer: "Robert Schumann" },
-  { title: "Le Lac des Cygnes", composer: "Piotr Ilitch Tchaïkovski" },
+
+  { title: "Nocturne n°2", composer: "Chopin" },
+
+  { title: "The entertainer", composer: "Scott Joplin" },
+  { title: "Mapple leaf rag", composer: "Scott Joplin" },
+
+  { title: "Forrest Gump", composer: "Alan Silvestri" },
+
+  { title: "To Zanarkand (Final Fantasy X)", composer: "Nobuo Uematsu" },
+
+  { title: "Main theme (Suzuka)", composer: "Masanori Takumi" },
+  { title: "Tooi Hi No Kizuato (Suzuka)", composer: "Masanori Takumi" },
+
+  { title: "Sadness and Sorrow (Naruto)", composer: "Toshio Masuda" },
+
+  { title: "Bella's Lullaby (Twilight)", composer: "Carter Burwell" },
+
+  { title: "Love me", composer: "Michel Polnareff" },
+
+  { title: "Logical Song", composer: "Supertramp" },
+
+  { title: "The Scientist", composer: "Coldplay" },
+  { title: "Trouble", composer: "Coldplay" },
+  { title: "Viva la vida", composer: "Coldplay" },
+  { title: "Clocks", composer: "Coldplay" },
+
+  { title: "Apologize", composer: "One Republic" },
+
+  { title: "Someone like you", composer: "Adèle" },
+
+  { title: "What I've done", composer: "Linkin Park" },
+
+  { title: "Yesterday", composer: "The Beatles" },
+
+  { title: "Kiss me slowly", composer: "Parachutes" },
+
+  { title: "My heart will go on (Titanic)", composer: "Céline Dion" },
+
+  { title: "Fireflies", composer: "Owl City" },
+
+  { title: "Chasing cars", composer: "Snow Patrol" },
+
+  { title: "Comme un ouragan", composer: "Stéphanie de Monaco" },
+
+  { title: "Demons", composer: "Imagine Dragons" },
+
+  { title: "La panthère rose", composer: "Henry Mancini" },
 ];
