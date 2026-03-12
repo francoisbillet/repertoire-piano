30cbf545dfca43854acf8c4a02258206c7f0cc02
François Billet <francois.billet.ext@veolia.com>
2026-03-12 13:54:57 +0100

    Setup Github page

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: "/repertoire-piano/",
   plugins: [
     react(),
     mode === "development" && componentTagger(),
